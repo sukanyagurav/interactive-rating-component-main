fb61d6542589684fae73016857152ccab026f5dc
Sukanya Gurav <sukanyagurav6@gmail.com>
2023-03-10 01:00:05 +0530

    html css

---
+++ script.js
@@ -0,0 +1,6 @@
+const ratings =document.querySelector('.rating-container');
+const form = document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
+console.log(ratings)
